209d0447011d4c8f9caffadac45378f1938d13fc
kemenydani <kemenydani93@gmail.com>
2018-09-04 15:03:06 +0200

    initial commit

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ error_reporting(E_ALL);
 
 define('__ROOT__', __DIR__ . './../');
 define('__CONFIG_DIR__', __ROOT__ . '/config/');
+define('__STORAGE_DIR__', __ROOT__ . '/storage/');
+define('__UPLOADS_DIR__', __STORAGE_DIR__ . '/uploads/');
 
 require '../vendor/autoload.php';
 require '../App/functions.php';
