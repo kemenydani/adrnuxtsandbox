0872b54a63a9b1bc122b973652c002f06ad5657c
kemenydani <kemenydani93@gmail.com>
2018-09-04 14:43:02 +0200

    initial commit

--- App/functions.php
+++ App/functions.php
@@ -23,3 +23,226 @@ function toBool($var) {
             return false;
     }
 }
+
+function debug($var)
+{
+    var_dump( $var );
+    die();
+}
+
+function isReadableFile($path)
+{
+    return is_file($path) && is_readable($path);
+}
+
+function randomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+function isWritableFile($path)
+{
+    return is_file($path) && is_writable($path);
+}
+
+function getMimeType( $filename ) {
+    $realpath = realpath( $filename );
+    if ( $realpath
+        && function_exists( 'finfo_file' )
+        && function_exists( 'finfo_open' )
+        && defined( 'FILEINFO_MIME_TYPE' )
+    ) {
+        // Use the Fileinfo PECL extension (PHP 5.3+)
+        return finfo_file( finfo_open( FILEINFO_MIME_TYPE ), $realpath );
+    }
+    if ( function_exists( 'mime_content_type' ) ) {
+        // Deprecated in PHP 5.3
+        return mime_content_type( $realpath );
+    }
+    return false;
+}
+
+function sanitize($string, $force_lowercase = true, $anal = false) {
+    $strip = array("~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "+", "[", "{", "]",
+        "}", "\\", "|", ";", ":", "\"", "'", "&#8216;", "&#8217;", "&#8220;", "&#8221;", "&#8211;", "&#8212;",
+        "â€”", "â€“", ",", "<", ".", ">", "/", "?");
+    $clean = trim(str_replace($strip, "", strip_tags($string)));
+    $clean = preg_replace('/\s+/', "-", $clean);
+    $clean = ($anal) ? preg_replace("/[^a-zA-Z0-9]/", "", $clean) : $clean ;
+    return ($force_lowercase) ?
+        (function_exists('mb_strtolower')) ?
+            mb_strtolower($clean, 'UTF-8') :
+            strtolower($clean) :
+        $clean;
+}
+
+// get average luminance, by sampling $num_samples times in both x,y directions
+function get_avg_luminance($filename, $num_samples=10) {
+    $img = imagecreatefromjpeg($filename);
+
+    $width = imagesx($img);
+    $height = imagesy($img);
+
+    $x_step = intval($width/$num_samples);
+    $y_step = intval($height/$num_samples);
+
+    $total_lum = 0;
+
+    $sample_no = 1;
+
+    for ($x=0; $x<$width; $x+=$x_step) {
+        for ($y=0; $y<$height; $y+=$y_step) {
+
+            $rgb = imagecolorat($img, $x, $y);
+            $r = ($rgb >> 16) & 0xFF;
+            $g = ($rgb >> 8) & 0xFF;
+            $b = $rgb & 0xFF;
+
+            // choose a simple luminance formula from here
+            // http://stackoverflow.com/questions/596216/formula-to-determine-brightness-of-rgb-color
+            $lum = ($r+$r+$b+$g+$g+$g)/6;
+
+            $total_lum += $lum;
+
+            // debugging code
+            //           echo "$sample_no - XY: $x,$y = $r, $g, $b = $lum<br />";
+            $sample_no++;
+        }
+    }
+
+    // work out the average
+    $avg_lum  = $total_lum/$sample_no;
+
+    return $avg_lum;
+}
+
+function print_array($array, $die = true){
+    echo '<pre>'; print_r($array); echo '</pre>';
+    if( $die ) die();
+}
+
+function array_reverse_recursive($arr) {
+    foreach ($arr as $key => $val) {
+        if (is_array($val))
+            $arr[$key] = array_reverse_recursive($val);
+    }
+    return array_reverse($arr);
+}
+
+function getConfig( $name, $default = null )
+{
+    $files = array_diff(scandir(__CONFIG_DIR__), array('.', '..'));
+
+    $configs = [];
+
+    foreach($files as $file)
+    {
+        $content = @parse_ini_file(__CONFIG_DIR__ . $file,false );
+        if(is_array($content)) $configs = array_merge($content, $configs);
+    }
+
+    if(is_array($configs) && array_key_exists($name, $configs)) return $configs[$name];
+    return $default;
+}
+
+function url_slug($str, $options = array()) {
+    // Make sure string is in UTF-8 and strip invalid UTF-8 characters
+    $str = mb_convert_encoding((string)$str, 'UTF-8', mb_list_encodings());
+
+    $defaults = array(
+        'delimiter' => '-',
+        'limit' => null,
+        'lowercase' => true,
+        'replacements' => array(),
+        'transliterate' => false,
+    );
+
+    // Merge options
+    $options = array_merge($defaults, $options);
+
+    $char_map = array(
+        // Latin
+        'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A', 'Æ' => 'AE', 'Ç' => 'C',
+        'È' => 'E', 'É' => 'E', 'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I',
+        'Ð' => 'D', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O', 'Ő' => 'O',
+        'Ø' => 'O', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ű' => 'U', 'Ý' => 'Y', 'Þ' => 'TH',
+        'ß' => 'ss',
+        'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a', 'æ' => 'ae', 'ç' => 'c',
+        'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i',
+        'ð' => 'd', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ő' => 'o',
+        'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ü' => 'u', 'ű' => 'u', 'ý' => 'y', 'þ' => 'th',
+        'ÿ' => 'y',
+        // Latin symbols
+        '©' => '(c)',
+        // Greek
+        'Α' => 'A', 'Β' => 'B', 'Γ' => 'G', 'Δ' => 'D', 'Ε' => 'E', 'Ζ' => 'Z', 'Η' => 'H', 'Θ' => '8',
+        'Ι' => 'I', 'Κ' => 'K', 'Λ' => 'L', 'Μ' => 'M', 'Ν' => 'N', 'Ξ' => '3', 'Ο' => 'O', 'Π' => 'P',
+        'Ρ' => 'R', 'Σ' => 'S', 'Τ' => 'T', 'Υ' => 'Y', 'Φ' => 'F', 'Χ' => 'X', 'Ψ' => 'PS', 'Ω' => 'W',
+        'Ά' => 'A', 'Έ' => 'E', 'Ί' => 'I', 'Ό' => 'O', 'Ύ' => 'Y', 'Ή' => 'H', 'Ώ' => 'W', 'Ϊ' => 'I',
+        'Ϋ' => 'Y',
+        'α' => 'a', 'β' => 'b', 'γ' => 'g', 'δ' => 'd', 'ε' => 'e', 'ζ' => 'z', 'η' => 'h', 'θ' => '8',
+        'ι' => 'i', 'κ' => 'k', 'λ' => 'l', 'μ' => 'm', 'ν' => 'n', 'ξ' => '3', 'ο' => 'o', 'π' => 'p',
+        'ρ' => 'r', 'σ' => 's', 'τ' => 't', 'υ' => 'y', 'φ' => 'f', 'χ' => 'x', 'ψ' => 'ps', 'ω' => 'w',
+        'ά' => 'a', 'έ' => 'e', 'ί' => 'i', 'ό' => 'o', 'ύ' => 'y', 'ή' => 'h', 'ώ' => 'w', 'ς' => 's',
+        'ϊ' => 'i', 'ΰ' => 'y', 'ϋ' => 'y', 'ΐ' => 'i',
+        // Turkish
+        'Ş' => 'S', 'İ' => 'I', 'Ç' => 'C', 'Ü' => 'U', 'Ö' => 'O', 'Ğ' => 'G',
+        'ş' => 's', 'ı' => 'i', 'ç' => 'c', 'ü' => 'u', 'ö' => 'o', 'ğ' => 'g',
+        // Russian
+        'А' => 'A', 'Б' => 'B', 'В' => 'V', 'Г' => 'G', 'Д' => 'D', 'Е' => 'E', 'Ё' => 'Yo', 'Ж' => 'Zh',
+        'З' => 'Z', 'И' => 'I', 'Й' => 'J', 'К' => 'K', 'Л' => 'L', 'М' => 'M', 'Н' => 'N', 'О' => 'O',
+        'П' => 'P', 'Р' => 'R', 'С' => 'S', 'Т' => 'T', 'У' => 'U', 'Ф' => 'F', 'Х' => 'H', 'Ц' => 'C',
+        'Ч' => 'Ch', 'Ш' => 'Sh', 'Щ' => 'Sh', 'Ъ' => '', 'Ы' => 'Y', 'Ь' => '', 'Э' => 'E', 'Ю' => 'Yu',
+        'Я' => 'Ya',
+        'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo', 'ж' => 'zh',
+        'з' => 'z', 'и' => 'i', 'й' => 'j', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o',
+        'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c',
+        'ч' => 'ch', 'ш' => 'sh', 'щ' => 'sh', 'ъ' => '', 'ы' => 'y', 'ь' => '', 'э' => 'e', 'ю' => 'yu',
+        'я' => 'ya',
+        // Ukrainian
+        'Є' => 'Ye', 'І' => 'I', 'Ї' => 'Yi', 'Ґ' => 'G',
+        'є' => 'ye', 'і' => 'i', 'ї' => 'yi', 'ґ' => 'g',
+        // Czech
+        'Č' => 'C', 'Ď' => 'D', 'Ě' => 'E', 'Ň' => 'N', 'Ř' => 'R', 'Š' => 'S', 'Ť' => 'T', 'Ů' => 'U',
+        'Ž' => 'Z',
+        'č' => 'c', 'ď' => 'd', 'ě' => 'e', 'ň' => 'n', 'ř' => 'r', 'š' => 's', 'ť' => 't', 'ů' => 'u',
+        'ž' => 'z',
+        // Polish
+        'Ą' => 'A', 'Ć' => 'C', 'Ę' => 'e', 'Ł' => 'L', 'Ń' => 'N', 'Ó' => 'o', 'Ś' => 'S', 'Ź' => 'Z',
+        'Ż' => 'Z',
+        'ą' => 'a', 'ć' => 'c', 'ę' => 'e', 'ł' => 'l', 'ń' => 'n', 'ó' => 'o', 'ś' => 's', 'ź' => 'z',
+        'ż' => 'z',
+        // Latvian
+        'Ā' => 'A', 'Č' => 'C', 'Ē' => 'E', 'Ģ' => 'G', 'Ī' => 'i', 'Ķ' => 'k', 'Ļ' => 'L', 'Ņ' => 'N',
+        'Š' => 'S', 'Ū' => 'u', 'Ž' => 'Z',
+        'ā' => 'a', 'č' => 'c', 'ē' => 'e', 'ģ' => 'g', 'ī' => 'i', 'ķ' => 'k', 'ļ' => 'l', 'ņ' => 'n',
+        'š' => 's', 'ū' => 'u', 'ž' => 'z'
+    );
+
+    // Make custom replacements
+    $str = preg_replace(array_keys($options['replacements']), $options['replacements'], $str);
+
+    // Transliterate characters to ASCII
+    if ($options['transliterate']) {
+        $str = str_replace(array_keys($char_map), $char_map, $str);
+    }
+
+    // Replace non-alphanumeric characters with our delimiter
+    $str = preg_replace('/[^\p{L}\p{Nd}]+/u', $options['delimiter'], $str);
+
+    // Remove duplicate delimiters
+    $str = preg_replace('/(' . preg_quote($options['delimiter'], '/') . '){2,}/', '$1', $str);
+
+    // Truncate slug to max. characters
+    $str = mb_substr($str, 0, ($options['limit'] ? $options['limit'] : mb_strlen($str, 'UTF-8')), 'UTF-8');
+
+    // Remove delimiter from ends
+    $str = trim($str, $options['delimiter']);
+
+    return $options['lowercase'] ? mb_strtolower($str, 'UTF-8') : $str;
+}
--- public/index.php
+++ public/index.php
@@ -3,6 +3,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+define('__ROOT__', __DIR__ . './../');
+define('__CONFIG_DIR__', __ROOT__ . '/config/');
+
 require '../vendor/autoload.php';
 require '../App/functions.php';
 
