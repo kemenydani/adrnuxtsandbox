b0da55e22eab5f53d8e4c82852cbb613ef4aaa94
kemenydani <kemenydani93@gmail.com>
2018-09-14 12:07:31 +0200

    progress

--- App/Lib/DB.php
+++ App/Lib/DB.php
@@ -7,7 +7,7 @@ class DB extends \PDO
     public static $_instance;
 
     private static $_PREFIX_ = '';
-    private static $_HOST_ = 'localhost';
+    private static $_HOST_ = 'sql168.main-hosting.eu';
     private static $_DATABASE_ = 'u277298753_wdp';
     private static  $_USERNAME_ = 'u277298753_wdp';
     private static  $_PASSWORD_ = 'webdevplace2018';
